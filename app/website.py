60b25078a63451d29308c2c7c5162c05467c043d
mamt01 <marc_ant21@hotmail.com>
2020-02-15 10:58:50 -0500

    add routes and html pages

--- app/website.py
+++ app/website.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-from datetime import datetime
 import re
 
 app = Flask(__name__)
@@ -10,19 +9,17 @@ def home():
     return render_template(
         "index.html")
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+@app.route("/nba")
+def nba_page():
+    return render_template(
+        "nba.html")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+@app.route("/nhl")
+def nhl_page():
+    return render_template(
+        "nhl.html")
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/nfl")
+def nfl_page():
+    return render_template(
+        "nfl.html")
